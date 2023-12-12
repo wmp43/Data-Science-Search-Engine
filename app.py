a1fa0c42a7308663b9a56580c6dc09469a47c9e7
wmp43 <wmp43@cornell.edu>
2023-12-11 21:49:43 -0800

    note taking

--- app.py
+++ app.py
@@ -27,5 +27,16 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+"""
+Status: 
+- Data Cleaning method is API call, split and clean, regex, return text
+- This pipeline lives in the jupyter notebook text_cleaning_test_1 and sans 1. 
+- Issues: need to figure out how to remove latex. Start with chunking at section headings
+- then figure out db. thinking about pgvector due to open source. Need host that allows for extensions
+
+
+"""
+
+
 if __name__ == '__main__':
     app.run()
