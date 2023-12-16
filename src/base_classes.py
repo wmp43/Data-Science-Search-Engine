5f693f0219bc5f9364c22bb4444f09d490261955
wmp43 <wmp43@cornell.edu>
2023-12-15 17:35:37 -0800

    article data class coming along. Each article object will be so large wowie

--- src/base_classes.py
+++ src/base_classes.py
@@ -16,10 +16,8 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from sentence_transformers import SentenceTransformer
 from nltk.tokenize import word_tokenize
-from xgboost import XGBClassifier
-from sklearn.decomposition import PCA
 from pydantic import BaseModel
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Optional, Tuple
 import uuid
 import os
 import re
@@ -84,20 +82,82 @@ class Category:
         self.return_items = returned_items
 
 
+
+
 @dataclass
 class Article:
     category: str
     title: str
     id: str
-    text: List[str]
-    metadata: Optional[List[Dict]]
-    embedding: Optional[List[List[float]]]
+    text: str
+    text_dict: Optional[Dict[str, List[Tuple[str, np.ndarray]]]]
+    metadata: Optional[Dict[str, List[Dict[str, any]]]]
     text_processor: TextProcessor = field(default=BaseTextProcessor())
+    """
+    {
+    section_heading: [(cleaned_text: str, embedding: np.ndarry[float64]), 
+                      (cleaned_text: str, embedding: np.ndarry[float64]),
+                      (cleaned_text: str, embedding: np.ndarry[float64])]
+    }
+    
+    Then each tuple from the values list can then build the vectorDB record:
+    
+    metadata_example = {
+        "Introduction": [
+            {
+                "part": 1,
+                "categories": ["Basic Concepts", "Overview"],
+                "mentioned_people": ["John Smith"],
+                "mentioned_places": ["Paris"],
+                "mentioned_topics": ["Introduction to Statistics"]
+            },
+            {
+                "part": 2,
+                "categories": ["Overview"],
+                "mentioned_people": ["Jane Doe"],
+                "mentioned_places": ["New York"],
+                "mentioned_topics": ["Historical Context"]
+            }
+        ],
+        "Methodology": [
+            {
+                "part": 1,
+                "categories": ["Research Methods"],
+                "mentioned_people": ["Alice Johnson"],
+                "mentioned_places": ["London"],
+                "mentioned_topics": ["Survey Design"]
+            },
+            {
+                "part": 2,
+                "categories": ["Data Analysis"],
+                "mentioned_places": ["Berlin"],
+                "mentioned_topics": ["Data Collection Techniques"]
+            }
+        ],
+    }
 
-    def process_text_pipeline(self, text_processor):
+Vector record
+    vector_record = {
+        "embedding": np.ndarray
+        "id": 1234,
+        "title": "title of article",
+        "metadata": {"section heading":{
+                "categories": ['bayesian_statistics', 'posterior_probability', 'bayes_estimation'],
+                "mentioned_people": ['william_bayes'],
+                "mentioned_places": ['london'],
+                "mentioned_topics": ['bayesian_economics', 'bayesian_deep_learning']
+                }
+        }
+    }
+    """
+
+    def process_text_pipeline(self, text_processor, exclude_section):
         # This should include a pipeline to process text
-        clean_text = text_processor.clean_text(self.text)
-        return clean_text
+        processor = text_processor()
+        text_dict = processor.build_section_dict(self, exclude_section)
+        clean_text_dict = processor.remove_curly_brackets(text_dict)
+        self.text_dict = clean_text_dict
+
 
     def update_categories(self, new_category):
         # If article found in new
@@ -136,7 +196,7 @@ class BaseTextProcessor(TextProcessor):
     [1] -> [many] -> [many cleaned] -> [[f32],[f32],[f32]]
     """
 
-    def build_section_dict(self, article: Article()) -> Dict:
+    def build_section_dict(self, article: Article(), exclude_sections: List) -> Dict:
         """
         Split on section headings
         :return: Dictionary with section headings as keys and text as value.
@@ -153,11 +213,14 @@ class BaseTextProcessor(TextProcessor):
         section_headers = re.findall(section_pattern, text)
         for i, header in enumerate(section_headers):
             clean_header = header.strip("= ").strip()
-            sections[clean_header] = parts[i + 1].strip() if i + 1 < len(parts) else ""
 
+            if clean_header not in exclude_sections:
+                sections[clean_header] = parts[i + 1].strip() if i + 1 < len(parts) else ""
         return sections
 
-    def remove_curly_brackets(self, section_dict):
+
+
+    def remove_curly_brackets(self, section_dict) -> Dict:
         """Removes the curly braces (latex) from the values in each section."""
         cleaned_sections = {}
 
@@ -182,19 +245,34 @@ class BaseTextProcessor(TextProcessor):
 
         return cleaned_sections
 
-    def build_embeddings(self, article: Article(), OAIapi: OpenAIAPI()):
+
+"""    
+For each text chunk, we should use langchain's functionality for text splitting and embedding
+- need to figure out 
+
+
+        {section: text, 
+        section: text, 
+        section: text,
+        section: text}
+    
+"""
+
+    def build_embeddings(self, article: Article(), OAIapi: OpenAIAPI(), section_dict):
         """
         :param article:
         :param OAIapi:
         :return:
         """
-        oai_api = OAIapi(token=openai_token, endpoint=openai_endpoint)
+        oai_api = OAIapi(token=1, endpoint=1)
         for idx, text in enumerate(article.text):
             article.embedding[idx] = list(oai_api.fetch_embeddings(text))
+        return 1
 
-    def build_metadata(self, article: Article()):
-        """" Go through each chunk and build some metadata !!!!
+    def build_metadata(self, article: Article(), section_dict):
+        """"
         :param article: article object containing text,
+        :param section_dict: dictionary with sections
         :return: dictionary with relevant metadata
         """
         for idx, text_chunk in enumerate(article.text):
@@ -205,7 +283,4 @@ class BaseTextProcessor(TextProcessor):
 
             article.metadata[idx] = dict
             # For each chunk of text
-
-
-
-# todo: get the response str to upsert pipeline correct Nov 29th
+        return None
