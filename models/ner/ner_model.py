36ec9bc58e11c1a4e9e006762f2549866d581a9f
wmp43 <wmp43@cornell.edu>
2023-12-23 16:23:54 -0700

    adding some files

--- models/ner/ner_model.py
+++ models/ner/ner_model.py
@@ -1,3 +1,7 @@
 """
-Goal: Develop a ner model to pick out topics, mentions, latex to remove
+Goal: Develop a ner model to pick out topics, mentions.
+
+Going to start with spaCy methods to just pick out strings. This is the quickest and easiest way.
+I am unsure if a full ner model is useful enough to justify when building out core.
+Will do it in future though
 """
