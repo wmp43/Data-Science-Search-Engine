db217fb6344109909d7709f8fd1778442299fb36
Myles Pember <wmp43@cornell.edu>
2024-01-16 00:45:46 -0800

    apply dim reduc and viz after clustering... will be much better

--- src/txt_clustering.py
+++ src/txt_clustering.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src.rds_crud import ArticleTable
 from config import rds_port, rds_host, rds_user, rds_password, rds_dbname
-from sklearn.decomposition import PCA
 import ast
-from sklearn.manifold import TSNE, Isomap, LocallyLinearEmbedding, MDS
+from sklearn.manifold import TSNE
 import plotly.express as px
-from sklearn.cluster import DBSCAN
-from sklearn import metrics
+from sklearn.cluster import DBSCAN, HDBSCAN, OPTICS
+from itertools import cycle
+
 
 
 
@@ -64,6 +64,7 @@ print(f'Euc mean distance on reduced dim dimensions: {mean_distance}')
 
 # Apply DBSCAN clustering
 """
+DBSCAN
 Arguments and results 
 eps = mean_distance * 0.1 || min_samples = 5
 - Good Baseline
@@ -71,24 +72,49 @@ eps = mean_distance * 0.1 || min_samples = 5
 eps = mean_distance * 1.2 || min_samples = 10
 - Also Good
 
+HDBSCAN
+
+
 
 
 """
-DB_SCAN = True
+DB_SCAN = False
+HDB_SCAN = True
+OPTIC = False
 if DB_SCAN:
-    db = DBSCAN(eps=mean_distance*0.10, min_samples=10).fit(reduced_dims)
-    labels = db.labels_
+    # eps=mean_distance*0.11, min_samples=10
+    db = DBSCAN().fit(reduced_dims)
+elif HDB_SCAN:
+    # cluster_selection_epsilon=0.10,  min_cluster_size=10
+    db = HDBSCAN().fit(reduced_dims)
+elif OPTIC:
+    db = OPTICS().fit(reduced_dims)
 
-    # Number of clusters in labels, ignoring noise if present.
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    n_noise_ = list(labels).count(-1)
+labels = db.labels_
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
 
-from itertools import cycle
-palette = cycle(px.colors.qualitative.Plotly)
-colors = [next(palette) for _ in range(n_clusters_)] + ['grey']
-fig = px.scatter_3d(df, x='dim_1', y='dim_2', z='dim_3', hover_name='title', color=labels, color_discrete_sequence=colors)
-fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-fig.show()
 
-print("Estimated number of clusters: %d" % n_clusters_)
-print("Estimated number of noise points: %d" % n_noise_)
+# Filter out noise points
+PLOT_NOISE = False
+if not PLOT_NOISE:
+    df_clustered = df[labels == -1]
+    labels_filtered = labels[labels == -1]
+    n_clusters_ = len(set(labels_filtered))
+    palette = cycle(px.colors.qualitative.Plotly)
+    colors = [next(palette) for _ in range(n_clusters_)]
+    fig = px.scatter_3d(df_clustered, x='dim_1', y='dim_2', z='dim_3', hover_name='title', color=labels_filtered, color_discrete_sequence=colors)
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+    fig.show()
+
+    print("Estimated number of clusters (excluding noise): %d" % n_clusters_)
+    print("Estimated number of noise points: %d" % n_noise_)
+else:
+    palette = cycle(px.colors.qualitative.Plotly)
+    colors = [next(palette) for _ in range(n_clusters_)] + ['grey']
+    fig = px.scatter_3d(df, x='dim_1', y='dim_2', z='dim_3', hover_name='title', color=labels, color_discrete_sequence=colors)
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+    fig.show()
+
+    print("Estimated number of clusters: %d" % n_clusters_)
+    print("Estimated number of noise points: %d" % n_noise_)
