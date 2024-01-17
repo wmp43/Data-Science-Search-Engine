5d4c21a1ac7cd2eb27fc3046771a4b5959cde866
Myles Pember <wmp43@cornell.edu>
2024-01-17 11:16:53 -0800

    building app.

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 from src.base_models import Query
-from src.query_processor import BaseQueryProcessor
+from src.query_processor import BaseQueryProcessor, QueryVisualizer
 from src.rds_crud import VectorTable
 from config import rds_user, rds_password, rds_host, rds_port, rds_dbname
 
 app = Flask(__name__)
 rds_args = (rds_dbname, rds_user, rds_password, rds_host, rds_port)
+vec_tbl = VectorTable(*rds_args)
+
+
+"""
+search_term = request.form['search_term']
+qp, qv = BaseQueryProcessor(), QueryVisualizer()
+query_obj = Query(search_term, qp, qv, vec_tbl, embedding=[], results=[])
+This Query Object can also be used for query visualization
+
+A Corpus visualization will be different.
+"""
 
 @app.route('/search', methods=['POST'])
 def search():
-    # user -> query -> expanded -> embedded -> results -> re ranked -> LLM Res
-    # this may be super slow....
-    bqp, vec_tbl = BaseQueryProcessor(), VectorTable(*rds_args)
-    input = Query(query, bqp)
-    expanded = input.expand_query().encode_query()
+    # Should go from query result to table and LLM response.
+    search_term = request.form['search_term']
+    qp, qv = BaseQueryProcessor(), QueryVisualizer()
+    query_obj = Query(search_term, qp, qv, vec_tbl, embedding=[], results=[])
+    query_obj.process()
+    query_obj.execute()
+
 
 
 
@@ -21,22 +34,6 @@ def search():
 def hello_world():  # put application's code here
     # Flask Endpoint For Wiki Search text input
     return 'Hello World!'
-
-
-@app.route('/versioning')
-def hello_world():  # put application's code here
-    # Flask Endpoint for doc versioning, upload
-    return 'Hello World!'
-
-"""
-Status: 
-- Data Cleaning method is API call, split and clean, regex, return text
-- This pipeline lives in the jupyter notebook text_cleaning_test_1 and sans 1. 
-- Issues: need to figure out how to remove latex. Start with chunking at section headings
-- then figure out db. thinking about pgvector due to open source. Need host that allows for extensions
-
-
-"""
 
 
 if __name__ == '__main__':
